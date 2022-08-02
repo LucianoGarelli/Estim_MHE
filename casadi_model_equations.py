c63c0f33deda8c7b3877e43077946fa0a408ebf7
Nicolas <nicolas.trivisonno@gmail.com>
2022-08-02 16:12:44 -0300

    Show model eq similar to paper F01

--- casadi_model_equations.py
+++ casadi_model_equations.py
@@ -29,8 +29,8 @@ def meas(_x, _p, _S, _diam):
     # return [y[0], y[1], y[2], y[3], y[4], y[5]]
     # print(qdy * _S * (-Cd * ca * cb + CL_alfa * (sb ** 2 + sa ** 2 * cb ** 2)))
     return [qdy * _S * (-Cd * ca * cb + CL_alfa * (sb ** 2 + sa ** 2 * cb ** 2)),
-            qdy * _S * (-Cd * sb - CL_alfa * (ca * sb * cb) - Cn_p_alfa * _p[4] * _diam * (sa * cb) / _p[0]),
-            qdy * _S * (-Cd * sa * cb - CL_alfa * (sa * ca * cb ** 2) + Cn_p_alfa * _p[4] * _diam * sb / _p[0]),
+            qdy * _S * (-Cd * sb + CL_alfa * (-ca * sb * cb) + Cn_p_alfa * _p[4] * _diam * (-sa * cb) / _p[0]),
+            qdy * _S * (-Cd * sa * cb + CL_alfa * (-sa * ca * cb ** 2) + Cn_p_alfa * _p[4] * _diam * sb / _p[0]),
             qdy * _S * _diam * (_p[4] * _diam / _p[0]) * Clp,
-            qdy * _S * _diam * (Cm_alfa * (sa * cb) - (_p[4] * _diam / _p[0]) * (-Cm_p_alfa) * sb + (_diam / _p[0]) * Cm_q * _p[5]),
-            qdy * _S * _diam * (-Cm_alfa * sb - (_p[4] * _diam / _p[0]) * (-Cm_p_alfa) * (sa * cb) + (_diam / _p[0]) * Cm_q * _p[6])]
+            qdy * _S * _diam * (Cm_alfa * (sa * cb) + (_p[4] * _diam / _p[0]) * Cm_p_alfa * (+sb) + (_diam / _p[0]) * Cm_q * _p[5]),
+            qdy * _S * _diam * (Cm_alfa * (-sb) + (_p[4] * _diam / _p[0]) * Cm_p_alfa * (+sa * cb) + (_diam / _p[0]) * Cm_q * _p[6])]
